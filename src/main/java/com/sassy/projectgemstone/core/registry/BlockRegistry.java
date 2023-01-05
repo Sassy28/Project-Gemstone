4392c54eeee37d2bfbdd859838266b23c7be36a1
Sassy28 <thiaspraem@gmail.com>
2023-01-05 17:27:20 +0100

    Added multiple gemstones & gemstone blocks

--- src/main/java/com/sassy/projectgemstone/core/registry/BlockRegistry.java
+++ src/main/java/com/sassy/projectgemstone/core/registry/BlockRegistry.java
@@ -14,4 +14,12 @@ public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ProjectGemstone.MODID);
 
     public static final RegistryObject<Block> AMBER_BLOCK = BLOCKS.register("amber_block", () -> new Block(BlockBehaviour.Properties.of(Material.AMETHYST, MaterialColor.COLOR_ORANGE).strength(5.0f, 6.0f).sound(SoundType.AMETHYST).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> ALMANDINE_BLOCK = BLOCKS.register("almandine_block", () -> new Block(BlockBehaviour.Properties.of(Material.AMETHYST, MaterialColor.COLOR_BROWN).strength(5.0f, 6.0f).sound(SoundType.AMETHYST).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> AQUAMARINE_BLOCK = BLOCKS.register("aquamarine_block", () -> new Block(BlockBehaviour.Properties.of(Material.AMETHYST, MaterialColor.COLOR_CYAN).strength(5.0f, 6.0f).sound(SoundType.AMETHYST).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> AZURITE_BLOCK = BLOCKS.register("azurite_block", () -> new Block(BlockBehaviour.Properties.of(Material.AMETHYST, MaterialColor.COLOR_PURPLE).strength(5.0f, 6.0f).sound(SoundType.AMETHYST).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> HEMATINE_BLOCK = BLOCKS.register("hematine_block", () -> new Block(BlockBehaviour.Properties.of(Material.AMETHYST, MaterialColor.COLOR_BLACK).strength(5.0f, 6.0f).sound(SoundType.AMETHYST).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> MALACHITE_BLOCK = BLOCKS.register("malachite_block", () -> new Block(BlockBehaviour.Properties.of(Material.AMETHYST, MaterialColor.COLOR_GREEN).strength(5.0f, 6.0f).sound(SoundType.AMETHYST).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> SPHENE_BLOCK = BLOCKS.register("sphene_block", () -> new Block(BlockBehaviour.Properties.of(Material.AMETHYST, MaterialColor.COLOR_LIGHT_GREEN).strength(5.0f, 6.0f).sound(SoundType.AMETHYST).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> URAVORITE_BLOCK = BLOCKS.register("uravorite_block", () -> new Block(BlockBehaviour.Properties.of(Material.AMETHYST, MaterialColor.COLOR_LIGHT_GREEN).strength(5.0f, 6.0f).sound(SoundType.AMETHYST).requiresCorrectToolForDrops()));
+
 }
